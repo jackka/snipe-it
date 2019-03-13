691e0539cc2c9f00223d8a4c4350aef97b95e350
Evgeny <jackki@mail.ru>
2019-03-13 17:23:38 +0300

    trans_lang Squashed commit of the following:
    commit 43f292e2caea8ef435b61d410f0fd7c92226f59c
    Author: Evgeny <jackki@mail.ru>
    Date:   Thu Feb 21 13:43:21 2019 +0300
    
        ranslations lang for merging to crowdin.com
        admin/hardware
        admin/asset_maintenances
        admin/locations
        admin/statuslabels
        admin/users
        general
    
        translations lang for merging  to crowdin.com
        /general  - > текущий - текущие
    
        translations lang (/general)
        $ -> руб. ->валюта // deprecated
        Администрирование
    
        translations lang admin/hardware for crowdin.com applying
        form.php (warranty expires, текущее -> исходное расположение)
    
    ef8f70f Evgeny on 13/03/19 at 16:44
    trans_lang chekin, checkout (Выдать, Вернуть)
        general
    translations lang (/general)
    (cherry picked from commit 36921c8)

--- resources/lang/ru/admin/asset_maintenances/form.php
+++ resources/lang/ru/admin/asset_maintenances/form.php
@@ -1,14 +1,14 @@
 <?php
 
     return [
-        'asset_maintenance_type' => 'Состояние обслуживания',
+        'asset_maintenance_type' => 'Тип обслуживания',
         'title'                  => 'Заголовок',
         'start_date'             => 'Дата начала',
         'completion_date'        => 'Дата окончания',
-        'cost'                   => 'Стоймость',
+        'cost'                   => 'Стоимость',
         'is_warranty'            => 'Расширенная гарантия',
         'asset_maintenance_time' => 'Время обслуживания (в днях)',
         'notes'                  => 'Примечания',
-        'update'                 => 'Обновить статус',
+        'update'                 => 'Редактировать обслуживание',
         'create'                 => 'Создать'
     ];
